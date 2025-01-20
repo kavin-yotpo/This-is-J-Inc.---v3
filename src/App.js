70bad8ac3a5cbdbb3701195d42194b39c4105bcb
Kavin-crew <abreganakavin@gmail.com>
2025-01-21 00:54:19 +0800

    add prod id

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
   let InstanceId_ReviewsTab = "";
 
   //Product ID
-  let ProductId = "";
+  let ProductId = "9736864694553";
 
   //Clipboard text here.
   let ClipBoardHeading = "Follow these steps to add this";
